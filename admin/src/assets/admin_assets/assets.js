01aa1ed430d12584933390183ed825bd3ed3e1fc
Mohammed Afjal Shaikh <108579031+Afzal91004@users.noreply.github.com>
2024-12-20 08:55:33 +0530

    Delete admin directory

--- admin/src/assets/admin_assets/assets.js
+++
@@ -1,9 +0,0 @@
-import logo from "./logo.png";
-import upload_area from "./upload_area.png";
-import parcel_icon from "./parcel_icon.svg";
-
-export const assets = {
-  logo,
-  upload_area,
-  parcel_icon,
-};
